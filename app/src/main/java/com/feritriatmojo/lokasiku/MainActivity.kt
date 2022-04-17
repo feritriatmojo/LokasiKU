ac938f83cef74d42d89ff9472244710dabc5943f
feri triatmojo <203310032@students.akakom.ac.id>
2022-04-17 14:59:14 +0700

    Initial Commit

--- app/src/main/java/com/feritriatmojo/lokasiku/MainActivity.kt
+++ app/src/main/java/com/feritriatmojo/lokasiku/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.feritriatmojo.lokasiku
-
+//TODO 1 : Mengimport data yang diperlukan dalam aplikasi
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
@@ -13,19 +13,24 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+
+//TODO 2 : Class MainActivity dengan variabel didalamnya
 class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var locationManager: LocationManager
     private lateinit var tvGpsLocation: TextView
     private val locationPermissionCode = 2
+    //TODO 3 : Memanggil class super onCreate untuk menyelesaikan pembuatan aktivitas
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TODO 4 : Merupakan judul aplikasi yang dibuat
         title = "LokasiKU"
         val button: Button = findViewById(R.id.getLocation)
         button.setOnClickListener {
             getLocation()
         }
     }
+    //TODO 5 : Untuk mengambil lokasi Terkini
     private fun getLocation() {
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
@@ -33,10 +38,12 @@ class MainActivity : AppCompatActivity(), LocationListener {
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
     }
+    //TODO 6 : Untuk merubah apabila lokasi pengguna berbeda
     override fun onLocationChanged(location: Location) {
         tvGpsLocation = findViewById(R.id.textView)
         tvGpsLocation.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude
     }
+    //TODO 7 : Untuk memberikan izin pada aplikasi
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == locationPermissionCode) {
